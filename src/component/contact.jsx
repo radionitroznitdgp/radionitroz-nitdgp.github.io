d8f63237599005ef7842689c9f1040ae57ed5962
Vedang Pancholi <vedangpancholi@gmail.com>
2021-08-14 23:16:05 +0530

    Minor Changes

--- src/component/contact.jsx
+++ src/component/contact.jsx
@@ -11,12 +11,12 @@ const contact = () => {
                     <div className="contact">
                         <div className="contactField">
                             <div>
-                                <input type="text" name="name" placeholder="YOUR NAME" />
-                                <input type="email" name="email" placeholder="YOUR EMAIL" />
-                                <input type="text" name="subject" placeholder="YOUR SUBJECT OF MAIL" />
+                                <input type="text" name="name" placeholder="YOUR NAME" autocomplete="off"/>
+                                <input type="email" name="email" placeholder="YOUR EMAIL" autocomplete="off"/>
+                                <input type="text" name="subject" placeholder="YOUR SUBJECT OF MAIL" autocomplete="off"/>
                             </div>
                             <div>
-                                <textarea name="msg" cols="80" rows="9" placeholder="YOUR MESSAGE"></textarea>
+                                <textarea name="msg" cols="80" rows="9" placeholder="YOUR MESSAGE" autocomplete="off"></textarea>
                             </div>
                         </div>
                         <div className="btn">
--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,15 +1,63 @@
 import React from 'react';
+import MailIcon from '@material-ui/icons/Mail';
+import PhoneIcon from '@material-ui/icons/Phone';
 
-const footer = ()=> {
-    return(
+import vedang from '../images/members/vp.jpg';
+import amool from '../images/members/Amool.jpg';
+
+const footer = () => {
+
+    const modal = ()=>{
+        document.querySelector(".webTeam").classList.toggle('hide')
+        console.log(document.querySelector(".webTeam"))
+    }
+
+    return (
         <>
-        <div className="footer">
-            <div className="footercont">
-                <p>© Copyright 2021 Radio Nitroz NITDGP</p>
-                <span>Made with by <button onClick={()=>console.log('click')}>Web-Team</button></span>
-                <span>© All Rights Reserved</span>
+            <div className="footer">
+                <div className="footercont">
+                    <p>© Copyright 2021 Radio Nitroz NITDGP</p>
+                    <span>Made with by <button onClick={() => modal()}>Web-Team</button></span>
+                    <span>© All Rights Reserved</span>
+                </div>
             </div>
-        </div>
+            <div className="webTeam hide" onClick={()=> modal()}>
+                <div className="teamcont">
+                    <h1>WEB TEAM</h1>
+                    <div className="teamcard">
+                        <div className="datacard">
+                            <img src={vedang} alt="Vedang Pancholi" />
+                            <div className="social-link">
+                                <a className="social" href="mailto:vp.19u10233@btech.nitdgp.ac.in">
+                                    <MailIcon />
+                                </a>
+                                <a className="social" href="tel:9831417775">
+                                    <PhoneIcon />
+                                </a>
+                            </div>
+                            <div className="member-detail">
+                                <h4><b>Vedang Pancholi</b> <span>Web Developer Head</span></h4>
+                            </div>
+                        </div>
+
+                        <div className="datacard">
+                            <img src={amool} alt="Amool Kuldiya" />
+                            <div className="social-link">
+                                <a className="social" href="amoolkumar10@gmail.com">
+                                    <MailIcon />
+                                </a>
+                                <a className="social" href="tel:9352095586">
+                                    <PhoneIcon />
+                                </a>
+                            </div>
+                            <div className="member-detail ">
+                                <h4><b>Amool Kuldiya</b> <span>Web Developer</span></h4>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
